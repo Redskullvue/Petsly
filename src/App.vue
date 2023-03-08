b2180cd2e638445b068ce3cad7427148960d5e0f
redskull422 <kofifa.pg@gmail.com>
2023-03-08 05:49:59 +0330

    Initial Project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import "./output.css";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
