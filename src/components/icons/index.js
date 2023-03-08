62a7aaa0e44a6dac4c923dd07f69d922a7f97ae4
redskull422 <kofifa.pg@gmail.com>
2023-03-08 12:15:19 +0330

    Update :  New Icons Added

---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+import iMenu from './iMenu.vue';
+import iAccount from './iAccount.vue';
+import iBell from './iBell.vue';
+import iInfo from './iInfo.vue'
+
+
+export {iMenu, iAccount, iBell, iInfo}
