6749df1892cff9ed5335ea48740cfbc6731820a5
redskull422 <kofifa.pg@gmail.com>
2023-03-08 12:15:34 +0330

    update : New Icons Added

---
+++ src/components/icons/iInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    width="28"
+    height="28"
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="w-6 h-6"
+  >
+    <path
+      d="M14 0C6.272 0 0 6.272 0 14C0 21.728 6.272 28 14 28C21.728 28 28 21.728 28 14C28 6.272 21.728 0 14 0ZM15.4 21H12.6V12.6H15.4V21ZM15.4 9.8H12.6V7H15.4V9.8Z"
+      fill="black"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "iInfo",
+};
+</script>
+
+<style></style>
