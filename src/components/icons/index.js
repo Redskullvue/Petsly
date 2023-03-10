074bc28b46a95360fa0a2632c7e8b8b327e72db0
redskull422 <kofifa.pg@gmail.com>
2023-03-11 03:00:12 +0330

    Update : New Icons Added

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,7 +1,9 @@
 import iMenu from './iMenu.vue';
 import iAccount from './iAccount.vue';
 import iBell from './iBell.vue';
-import iInfo from './iInfo.vue'
+import iInfo from './iInfo.vue';
+import IGps from './iGps.vue';
+import iBack from './iBack.vue';
+import ILocation from './iLocation.vue'
 
-
-export {iMenu, iAccount, iBell, iInfo}
+export {iMenu, iAccount, iBell, iInfo , IGps, iBack,ILocation}
