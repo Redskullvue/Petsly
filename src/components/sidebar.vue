c9f08a637cfbbb2be239524c23e3806738d2da6d
redskull422 <kofifa.pg@gmail.com>
2023-03-11 13:39:30 +0330

    Fix : Side Bar animation fixed

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,43 +1,45 @@
 <template>
   <div
-    v-if="status"
-    class="bg-white shadow-lg w-2/3 h-screen fixed top-0 !z-50 md:w-1/3 transition-all"
+    :class="status ? '-translate-x-0' : 'translate-x-full'"
+    class="transition-all ease-in-out duration-700 fixed top-0 w-2/3 md:w-1/3"
   >
-    <div
-      class="bg-black rounded-br-2xl shadow-gray-400 shadow-lg rounded-bl-2xl flex items-center justify-center p-10 relative"
-    >
-      <button
-        @click="checkStatus"
-        class="absolute font-bold right-2 top-2 rounded-lg transition ease-in-out bg-white hover:-translate-y-1 hover:scale-110 duration-300"
+    <div v-if="status" class="bg-white shadow-lg h-screen !z-50">
+      <div
+        class="bg-black rounded-br-2xl shadow-gray-400 shadow-lg rounded-bl-2xl flex items-center justify-center p-10 relative"
       >
-        <i-back />
-      </button>
-      <img
-        class="rounded-full w-28 h-28 m-2"
-        src="../assets/images/Petshop.png"
-        alt="Pic"
-      />
-    </div>
-    <div class="mt-6 p-4">
-      <ul class="flex gap-y-2 flex-col">
-        <li
-          class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
+        <button
+          @click="checkStatus"
+          class="absolute font-bold right-2 top-2 rounded-lg transition ease-in-out bg-white hover:-translate-y-1 hover:scale-110 duration-300"
         >
-          <span><i-account /></span>پروفایل
-        </li>
-        <li
-          class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
-        >
-          <i-bell />
-          پرداخت ها
-        </li>
-        <li
-          class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
-        >
-          <i-info />
-          درباره ما
-        </li>
-      </ul>
+          <i-back />
+        </button>
+        <img
+          class="rounded-full w-28 h-28 m-2"
+          src="../assets/images/Petshop.png"
+          alt="Pic"
+        />
+      </div>
+      <div class="mt-6 p-4">
+        <ul class="flex gap-y-2 flex-col">
+          <li
+            class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
+          >
+            <span><i-account /></span>پروفایل
+          </li>
+          <li
+            class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
+          >
+            <i-bell />
+            پرداخت ها
+          </li>
+          <li
+            class="flex items-center gap-x-4 h-10 hover:cursor-pointer hover:font-bold"
+          >
+            <i-info />
+            درباره ما
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
